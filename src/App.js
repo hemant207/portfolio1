158fa5416b245d6b5fd6ef2af205926a1f0c39a1
hemant207 <hemantmangukiya93@gmail.com>
2023-09-12 21:24:43 +0100

    modify:json

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navigation from './component/Navigation';
 import Animation from './component/Animation';
 import About from './component/About';
 import Work from './component/Work';
 import Contact from './component/Contact';
-import Header from './component/Header';
+
 
 function App() {
   return (
     <div className="App">
-      {//<Header />
-}
+  
       <Animation />
       <About />
       <Work />
